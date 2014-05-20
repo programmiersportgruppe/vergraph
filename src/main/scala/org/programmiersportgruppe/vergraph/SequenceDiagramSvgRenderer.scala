48aa5e2f8ea22c00ffa4c1a59c05e8d15c0dec18
Ben Barnard <barnardb@gmail.com>
2014-05-21 01:31:17 +0200

    Tidy up SVG output

--- src/main/scala/org/programmiersportgruppe/vergraph/SequenceDiagramSvgRenderer.scala
+++ src/main/scala/org/programmiersportgruppe/vergraph/SequenceDiagramSvgRenderer.scala
@@ -37,19 +37,20 @@ case class Vector(x: Double, y: Double) {
 
 class SequenceDiagramSvgRenderer {
     def render(diagram: SequenceDiagram): Elem = {
-        val participantPosition: Map[String, Vector] = diagram.participants.zipWithIndex.toMap.mapValues(i => Vector(i * 100 + 50, 15))
-        <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" viewBox="0 0 600 238" shape-rendering="geometricPrecision" stroke-width="1" text-rendering="geometricPrecision" width="100%" height="100%">
+        val interParticipantSpace = 200.0
+        val participantPosition: Map[String, Vector] = diagram.participants.zipWithIndex.toMap.mapValues(i => Vector((i + 0.5) * interParticipantSpace, 15))
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" shape-rendering="geometricPrecision" stroke-width="1" text-rendering="geometricPrecision" width="100%" height="100%">
             {diagram.participants.map { participant =>
                 val pos = participantPosition(participant)
                 <g transform={s"${pos.translation}"}>
-                    <text style="text-anchor: middle" fill="#124191" text-anchor="middle" id="text4301">{participant}</text>
-                    <line xmlns="http://www.w3.org/2000/svg" x1="0" y1="5" x2="0" y2={(50 * diagram.events.size).toString} stroke="black" id="line4329"/> ++
+                    <text style="text-anchor: middle" fill="#124191" text-anchor="middle">{participant}</text>
+                    <line x1="0" y1="5" x2="0" y2={(50 * diagram.events.size).toString} stroke="black"/>
                 </g>
             }}
             {diagram.events.zipWithIndex.map { case (Message(from, to, message), index) =>
                 def offset(y: Int) = (index * 50 + y).toString
                 <g transform={s"translate(0 ${offset(30)})"}>
-                    <text dx={(participantPosition(from) + (participantPosition(to) - participantPosition(from)) * 0.5).x.toString} dy="12" style="text-anchor: middle" fill="#124191" text-anchor="middle" id="text4301">{message}</text>
+                    <text dx={(participantPosition(from) + (participantPosition(to) - participantPosition(from)) * 0.5).x.toString} dy="12" style="text-anchor: middle" fill="#124191" text-anchor="middle">{message}</text>
                     {arrow(participantPosition(from), participantPosition(to))}
                 </g>
             }}
@@ -58,14 +59,14 @@ class SequenceDiagramSvgRenderer {
 
     def arrow(from: Vector, to: Vector): Elem = {
         <g>
-            <line xmlns="http://www.w3.org/2000/svg" x1={from.x.toString} y1={from.y.toString} x2={to.x.toString} y2={to.y.toString} stroke="black" id="line4329"/> ++
+            <line x1={from.x.toString} y1={from.y.toString} x2={to.x.toString} y2={to.y.toString} stroke="black"/>
             {
                 val head = to
                 val direction = to - from
                 val top = Vector(10, 6)
                 val bottom = Vector(10, -6)
                 val angle = Math.atan2(direction.y, direction.x) * (180 / Math.PI) - 180
-                <polygon xmlns="http://www.w3.org/2000/svg" fill="black" transform={s"${head.translation} rotate($angle)"} points={s"0,0 $top $bottom"}/>
+                <polygon fill="black" transform={s"${head.translation} rotate($angle)"} points={s"0,0 $top $bottom"}/>
             }
         </g>
     }
